ea4adc8e41bd3b5cbb13958f487643947f399701
BautistaCharles <123444428+BautistaCharles@users.noreply.github.com>
2023-09-08 07:41:01 +0800

    Add files via upload

---
+++ Sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[] = {5, 4, 3, 2, 1};
+    int length = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = i + 1; j < length; j++) {
+            if (numbers[i] > numbers[j]) {
+                int temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < length; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
