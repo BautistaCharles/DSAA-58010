0cf59bf589ab1f2d38913e03354900e5b3753afe
BautistaCharles <123444428+BautistaCharles@users.noreply.github.com>
2023-09-08 07:29:14 +0800

    Add files via upload

---
+++ Append.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int length = sizeof(numbers) / sizeof(numbers[0]);
+
+    numbers[length - 0] = 11;
+
+    for (int i = 0; i < length; ++i) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
