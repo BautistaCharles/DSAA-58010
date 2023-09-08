802fe6aa0d92a5e9f419bd246c9f870baa72bb9d
BautistaCharles <123444428+BautistaCharles@users.noreply.github.com>
2023-09-08 08:05:37 +0800

    Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -3,20 +3,18 @@ using namespace std;
 
 int main() {
     int numbers[] = {5, 4, 3, 2, 1};
-    int length = sizeof(numbers) / sizeof(numbers[0]);
 
-    for (int i = 0; i < length - 1; i++) {
-        for (int j = i + 1; j < length; j++) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
             if (numbers[i] > numbers[j]) {
-                int temp = numbers[i];
-                numbers[i] = numbers[j];
-                numbers[j] = temp;
+                swap(numbers[i], numbers[j]);
             }
         }
     }
 
-    for (int i = 0; i < length; i++) {
-        cout<<numbers[i]<<" ";
+    for (int i = 0; i < 5; i++) {
+      cout<<numbers[i]<<" ";
     }
+
     return 0;
 }
